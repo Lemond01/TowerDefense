041dcb868bf48f744dd222732742c1ad1e105c03
Pochogame <cocoloco15.odvp@gmail.com>
2025-07-17 11:52:31 -0600

    poniendo las cosas en el demo

--- Assets/Scripts (Code)/Oscar/Scripts/WaveProgress.cs
+++ Assets/Scripts (Code)/Oscar/Scripts/WaveProgress.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class WaveProgressBar : MonoBehaviour
+public class WaveProgress : MonoBehaviour
 {
     [Header("Referencias UI")]
     public Slider   waveSlider;
